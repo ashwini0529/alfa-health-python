a14b9480c5fb874a01210a1db60651ca79f49ada
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-07-16 22:11:22 +0530

    Sample json added.

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ app.secret_key = os.urandom(24)
 @app.route('/<reg>')
 def dump(reg):
     error = None
-    url = 'http://apius.faceplusplus.com/v2/detection/detect?api_key=e2707513a30c55f950583457e8845ec1&api_secret=9cWd6oDOtFMmqhGT7mwPKphefakx52tI&url=https%3A%2F%2Facademics.vit.ac.in%2Fstudent%2Fview_photo_2.asp%3Frgno%3D'+str(reg)+'&attribute=age%2Cgender%2Crace%2Csmiling%2Cpose%2Cglass'
+    url = 'https://api.myjson.com/bins/4f6yu'
     r = requests.get(url)
     return json.dumps(r.json(), indent = 4)
 
