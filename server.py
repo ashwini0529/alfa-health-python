2031354cd4b976a0a72010a556419485e66d58fb
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-08-09 15:58:58 +0530

    sample exercise added

--- server.py
+++ server.py
@@ -67,6 +67,7 @@ def exercise():
 		url = "https://api.myjson.com/bins/4mua8"
 		r=requests.get(url)
 		return json.dumps(r.json(), indent=4)
+	
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
     ## keep the debug mode on in flask - it helps
