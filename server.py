c1e30a3493f28bbc0a17368c98e35b11572356b7
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-08-30 00:53:53 +0530

    Added BMI and BMR api

--- server.py
+++ server.py
@@ -1,13 +1,31 @@
-from flask import Flask, session, render_template
+from flask import Flask, jsonify, session, render_template, g
 ## you did not import "request"
 from flask import request
 import requests
 import json
 import os
+import sqlite3
 
+DATABASE = 'content.db'
 app = Flask(__name__)
+DEBUG = True
 
 app.secret_key = os.urandom(24)
+app.config.from_object(__name__)
+app.config.from_envvar('TRAVELSAFE_SETTINGS', silent=True)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
 
 @app.route('/')
 def dump():
@@ -22,7 +40,7 @@ def profile():
 	url = 'https://api.myjson.com/bins/59mna'
 	r = requests.get(url)
 	return json.dumps(r.json(), indent = 4)
-	
+
 @app.route('/exerciseList')
 def exerciseList():
 	error = None
@@ -39,35 +57,31 @@ def diet():
 def foo():
 	## the get request should be "/calculate?age=23&wight=99"
 	return str(request.args.get("age",type=int)) + str(request.args.get("weight",type=int))
-	
-@app.route('/exerciseSample', methods=['GET'])
-def exercise():
-	num=request.args.get("id", type=int)
-	if(num==1):
-		url = "https://api.myjson.com/bins/4xk34"
-		r=requests.get(url)
-		return json.dumps(r.json(), indent=4)
-	if(num==2):
-		url = "http://api.myjson.com/bins/3su1c"
-		r=requests.get(url)
-		return json.dumps(r.json(), indent=4)
-	if(num==3):
-		url = "https://api.myjson.com/bins/vt68"
-		r=requests.get(url)
-		return json.dumps(r.json(), indent=4)
-	if(num==4):
-		url = "https://api.myjson.com/bins/24tjk"
-		r=requests.get(url)
-		return json.dumps(r.json(), indent=4)
-	if(num==5):
-		url = "https://api.myjson.com/bins/103hs"
-		r=requests.get(url)
-		return json.dumps(r.json(), indent=4)
-	if(num==6):
-		url = "https://api.myjson.com/bins/4mua8"
-		r=requests.get(url)
-		return json.dumps(r.json(), indent=4)
-	
+@app.route('/try', methods=['GET'])
+def variable():
+	## the get request should be "/calculate?age=23&wight=99"
+	height = request.args.get('height', 0, type=float)
+    	weight = request.args.get('weight', 0, type=float)
+    	age = request.args.get('age', 0, type=int)
+    	gender = request.args.get('gender', 0, type=str)
+    	if(gender=='male'):
+            bmr=((10*weight)+(6.25*height)-(5*age)+5)
+        elif(gender=='female'):
+            bmr=((10*weight)+(6.25*height)-(5*age)-161)
+        height=height/100
+        ht=height*height
+        bmi=(weight/(ht))
+    	return jsonify(bmi= bmi,bmr=bmr)
+
+@app.route('/workout')
+def work():
+    sel = g.db.execute('select * from workout limit 5')
+    lis = ''
+    for row in sel.fetchall():
+        lis = lis + row[1] + ' ' + row[2] + ' ' + row[3] + ' ' + row[5] + ' ' + row[8] + ' ' + row[11]
+        lis = lis+'<br />'
+    return lis
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT',5000))
     ## keep the debug mode on in flask - it helps
