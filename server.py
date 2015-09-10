e27589a354bb2b68943b6d43aabc5431299e2bb2
Ashwini Purohit <ashwinipurohit@yahoo.in>
2015-09-11 02:47:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -115,6 +115,6 @@ def exer():
     return 'yo'
 ########################################################
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT',5002))
+    port = int(os.environ.get('PORT',5000))
     ## keep the debug mode on in flask - it helps
     app.run(host='0.0.0.0', port= port, debug=True)
